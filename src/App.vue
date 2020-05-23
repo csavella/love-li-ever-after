33ee96cedf2564b24d255c3b57d20e934251defe
Chasity Savella <csavella@pdx.edu>
2020-05-23 14:55:42 -0700

    Adding basic countdown timer

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,70 @@
           </ul>
         </div>
     </nav>
-    <router-view></router-view>
-  </div>
+    <router-view :time="times"></router-view>
+</div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+    startTime: Date.now(),
+    endTime: "Sept 18, 2021 17:00:00",
+    times: [
+      { id: 0, text: "Days", time: 1 },
+      { id: 1, text: "Hours", time: 1 },
+      { id: 2, text: "Minutes", time: 1 },
+      { id: 3, text: "Seconds", time: 1 }
+    ],
+    progress: 100,
+    // isActive: false,
+    timeinterval: undefined
+    }
+  },
+  methods: {
+    updateTimer: function() {
+      if (
+        this.times[3].time > 0 ||
+        this.times[2].time > 0 ||
+        this.times[1].time > 0 ||
+        this.times[0].time > 0
+      ) {
+        this.getTimeRemaining();
+        this.updateProgressBar();
+      } else {
+        clearTimeout(this.timeinterval);
+        // this.times[3].time = this.times[2].time = this.times[1].time = this.times[0].time = 0;
+         this.progress = 0;
+      }
+    },
+    getTimeRemaining: function() {
+      let t = Date.parse(new Date(this.endTime)) - Date.parse(new Date());
+      if(t >= 0){
+      this.times[3].time = Math.floor(t / 1000 % 60); //seconds
+      this.times[2].time = Math.floor(t / 1000 / 60 % 60); //minutes
+      this.times[1].time = Math.floor(t / (1000 * 60 * 60) % 24); //hours
+      this.times[0].time = Math.floor(t / (1000 * 60 * 60 * 24)); //days
+       }else {
+         this.times[3].time = this.times[2].time = this.times[1].time = this.times[0].time = 0;
+         this.progress = 0;
+       }
+    },
+    updateProgressBar: function() {
+      let startTime = Date.parse(new Date(this.startTime));
+      let currentTime = Date.parse(new Date());
+      let endTime = Date.parse(new Date(this.endTime));
+      let interval = parseFloat(
+        (currentTime - startTime) / (endTime - startTime) * 100
+      ).toFixed(2);
+      this.progress = 100-interval;
+    }
+  },
+  created: function() {
+    this.updateTimer();
+    this.timeinterval = setInterval(this.updateTimer, 1000);
+  }
+}
 </script>
 
 <style>
@@ -52,6 +111,14 @@ li {
 
 body {
   background-color: #c6dbf1;
+}
+
+.card-title{
+  padding: 1em;
+}
+.card {
+  padding-bottom: ;
+  margin-bottom: 2em;
 }
 
 </style>
---
+++ src/components/main.vue
@@ -0,0 +1,25 @@
+<template>
+  <table>
+      <tr>
+        <th>days</th>
+        <th>hours</th>
+        <th>min</th>
+        <th>sec</th>
+      </tr>
+      <tr>
+        <td>{{time[0].time}}</td>
+        <td>{{time[1].time}}</td>
+        <td>{{time[2].time}}</td>
+        <td>{{time[3].time}}</td>
+      </tr>
+  </table>
+</template>
+
+<script>
+export default {
+    name: 'main',
+    props: {
+      time: []
+    }
+}
+</script>
