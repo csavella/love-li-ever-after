f4aa906afd2737d29e3532431f181b57540631d4
Chasity Savella <csavella@pdx.edu>
2020-05-24 13:49:45 -0700

    Basic ceremony and reception details

--- src/components/details.vue
+++ src/components/details.vue
@@ -1,5 +1,36 @@
 <template>
-<div id="app">
-    Hello! You're viewing our wedding details!
-</div>
+  <div class="container details">
+    <section class="ceremony">
+        <h2>Ceremony and Reception</h2>
+        SATURDAY, SEPTEMBER 18, 2021<br>
+        5:00 - 10:00 PM<br>
+        <h3>Olowalu Plantation House</h3>
+        810 Olowalu Village Road<br>
+        Lahaina, Hawaii 96761<br>
+        <button class="btn btn-info" @click="newTab()">View Map</button>
+    </section>
+  </div>
 </template>
+
+<script>
+export default {
+    name: 'details',
+    methods: {
+      newTab: function() {
+        window.open("https://www.google.com/maps/place/Olowalu+Plantation+House/@20.8103194,-156.6257859,17z/data=!3m1!4b1!4m5!3m4!1s0x7954d627207ca707:0x77460d251222ff48!8m2!3d20.8103194!4d-156.6235972", "_blank");
+      }
+    }
+}
+</script>
+
+<style>
+.details {
+    padding: 20px;
+}
+.ceremony{
+    padding: 50px;
+}
+button {
+    padding: 10px;
+}
+</style>
