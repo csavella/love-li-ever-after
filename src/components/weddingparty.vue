e702961a00e0ebe2256eb4b18414f150a30af23f
Chasity Savella <csavella@pdx.edu>
2020-05-25 12:44:38 -0700

    Initial commit for party page
    2 sections with smooth scroll, placeholder text

--- src/components/weddingparty.vue
+++ src/components/weddingparty.vue
@@ -1,6 +1,58 @@
 <template>
-<div id="app">
-    Hello! You're viewing our wedding party!
-</div>
+    <div id="party">
+        <ul class="smooth-scroll list-unstyled partyintro">
+            <li>
+                <h5><a href="#groomsmen">Groomsmen</a></h5>
+            </li>
+            <br>
+            <li>
+                <h5><a href="#bridesmaids">Bridesmaids</a></h5>
+            </li>
+            <br>
+        </ul>
+
+        <!--Dummy sections with IDs coressponding with the links above-->
+        <section id="groomsmen">
+            <h3>Section 1</h3>
+            <hr>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+        </section>
+
+
+        <section id="bridesmaids">
+            <h3>Section 2</h3>
+            <hr>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+            <h5>Smooth Scroll Example</h5>
+        </section>
+    </div>
 </template>
 
+<style>
+.partyintro {
+    width: 100vw;
+    height: 100vh;
+}
+#groomsmen {
+    width: 100vw;
+    height: 100vh;
+}
+#bridesmaids {
+    width: 100vw;
+    height: 100vh;
+}
+
+</style>
