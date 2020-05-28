b1a63a918788313a6b6db24371cc77f62110274c
Chasity Savella <csavella@pdx.edu>
2020-05-27 23:27:07 -0700

    Getting font awesome to work - adding a few icons

--- src/components/planning.vue
+++ src/components/planning.vue
@@ -1,6 +1,55 @@
 <template>
-<div id="app">
-    Hello! You're viewing our planning details!
+<div id="planning">
+    <div class="my-5">
+        <section class="dark-grey-text text-center">
+            <h6 class="font-weight-normal text-uppercase font-small grey-text mb-4">Features</h6>
+            <h3 class="font-weight-bold black-text mb-4 pb-2">Unique Factors</h3>
+            <hr class="w-header">
+                <p class="lead text-muted mx-auto mt-4 pt-2 mb-5">It's really easy to create a landing page for your awesome product.</p>
+                <div class="row">
+                    <div class="col-md-3 mb-4">
+                        <a href="#!" class="card hoverable">
+                            <div class="card-body my-4">
+                                <p><i class="fas fa-tablet-alt fa-2x text-muted"></i></p>
+                                <!--<font-awesome-icon icon="tasks" />-->
+                                <font-awesome-icon :icon="['fas', 'tasks']" />
+                                <h5 class="black-text mb-0">Responsive</h5>
+                            </div>
+                        </a>
+                    </div>
+                <div class="col-md-3 mb-4">
+                    <a href="#!" class="card hoverable">
+                        <div class="card-body my-4">
+                            <p><i class="fas fa-cogs fa-2x text-muted"></i></p>
+                            <font-awesome-icon :icon="['fas', 'users']" />
+                            <h5 class="black-text mb-0">Customizable</h5>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 mb-4">
+                    <a href="#!" class="card hoverable">
+                        <div class="card-body my-4">
+                            <p><i class="fas fa-pencil-ruler fa-2x text-muted"></i></p>
+                            <font-awesome-icon :icon="['fas', 'money-bill']" />
+                            <h5 class="black-text mb-0">UI Elements</h5>            
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3 mb-4">
+                    <a href="#!" class="card hoverable">            
+                        <div class="card-body my-4">
+                            <p><i class="fas fa-recycle fa-2x text-muted"></i></p>
+                            <font-awesome-icon :icon="['fas', 'utensils']" />
+                            <h5 class="black-text mb-0">Clean Code</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section> 
+    </div>
 </div>
 </template>
+
+<script>
+</script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTasks } from '@fortawesome/free-solid-svg-icons'
+import { faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faMoneyBill } from '@fortawesome/free-solid-svg-icons'
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(faTasks,faUsers,faMoneyBill,faUtensils)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
