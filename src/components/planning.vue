36fbcf54cc1d293679bd85b6c9494d66060655e3
Chasity Savella <csavella@pdx.edu>
2020-05-28 19:41:27 -0700

    Checklist with pagination for task list section

--- src/components/planning.vue
+++ src/components/planning.vue
@@ -8,7 +8,7 @@
             <div class="col-md-3 mb-4 pt">
                 <a href="#deadlines" class="card hoverable planningtopic4">            
                     <div class="card-body my-4">
-                        <font-awesome-icon :icon="['fas', 'utensils']" size="2x"/>
+                        <font-awesome-icon :icon="['fas', 'clock']" size="2x"/>
                         <h5 class="black-text mb-0">Deadlines</h5>
                     </div>
                 </a>
@@ -50,7 +50,7 @@
             <div class="col-md-3 mb-4 pt">
                 <a href="#partysection" class="card hoverable planningtopic4">            
                     <div class="card-body my-4">
-                        <font-awesome-icon :icon="['fas', 'utensils']" size="2x"/>
+                        <font-awesome-icon :icon="['fas', 'glass-cheers']" size="2x"/>
                         <h5 class="black-text mb-0">Wedding Party</h5>
                     </div>
                 </a>
@@ -64,6 +64,121 @@
     </section>
     <section id="tasks">
         <h4 class="font-weight-bold black-text mb-4 pb-2">Tasks</h4>
+        <div class="row">
+            <div class="col-12">
+                <div class="card card-list">
+                    <div class="card-header white d-flex justify-content-between align-items-center py-3">
+                        <p class="h5-responsive font-weight-bold mb-0"><i class="fas fa-clipboard-list pr-2"></i>To Do List</p>
+                        <nav aria-label="Page navigation example">
+                        <ul class="pagination pg-blue mb-0">
+                            <li class="page-item">
+                            <a class="border page-link" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            </li>
+                            <li class="page-item"><a class="border page-link">1</a></li>
+                            <li class="page-item"><a class="border page-link">2</a></li>
+                            <li class="page-item"><a class="border page-link">3</a></li>
+                            <li class="page-item">
+                            <a class="border page-link" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                            </li>
+                        </ul>
+                        </nav>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-unstyled mb-0">
+                        <li class="d-flex justify-content-between align-items-center py-2 border-bottom">
+                            <div class="d-inline-flex">
+                            <div class="form-check pl-0">
+                                <input type="checkbox" class="form-check-input" id="materialChecked2">
+                                <label class="form-check-label" for="materialChecked2"></label>
+                            </div>
+                            <p class="mb-0"><span class="text">Design a nice theme</span></p>
+                            </div>
+                            <div class="tools">
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'edit']"/></a>
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'trash-alt']"/></a>
+                            </div>
+                        </li>
+                        <li class="d-flex justify-content-between align-items-center py-2 border-bottom">
+                            <div class="d-inline-flex">
+                            <div class="form-check pl-0">
+                                <input type="checkbox" class="form-check-input" id="materialChecked3">
+                                <label class="form-check-label" for="materialChecked3"></label>
+                            </div>
+                            <p class="mb-0"><span class="text">Make the theme responsive</span></p>
+                            </div>
+                            <div class="tools">
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'edit']"/></a>
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'trash-alt']"/></a>
+                            </div>
+                        </li>
+                        <li class="d-flex justify-content-between align-items-center py-2 border-bottom">
+                            <div class="d-inline-flex">
+                            <div class="form-check pl-0">
+                                <input type="checkbox" class="form-check-input" id="materialChecked4">
+                                <label class="form-check-label" for="materialChecked4"></label>
+                            </div>
+                            <p class="mb-0"><span class="text">Let theme shine like a star</span></p>
+                            </div>
+                            <div class="tools">
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'edit']"/></a>
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'trash-alt']"/></a>
+                            </div>
+                        </li>
+                        <li class="d-flex justify-content-between align-items-center py-2 border-bottom">
+                            <div class="d-inline-flex">
+                            <div class="form-check pl-0">
+                                <input type="checkbox" class="form-check-input" id="materialChecked5">
+                                <label class="form-check-label" for="materialChecked5"></label>
+                            </div>
+                            <p class="mb-0"><span class="text">Let theme shine like a star</span></p>
+                            </div>
+                            <div class="tools">
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'edit']"/></a>
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'trash-alt']"/></a>
+                            </div>
+                        </li>
+                        <li class="d-flex justify-content-between align-items-center py-2 border-bottom">
+                            <div class="d-inline-flex">
+                            <div class="form-check pl-0">
+                                <input type="checkbox" class="form-check-input" id="materialChecked6">
+                                <label class="form-check-label" for="materialChecked6"></label>
+                            </div>
+                            <p class="mb-0"><span class="text">Check your messages and notifications</span></p>
+                            </div>
+                            <div class="tools">
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'edit']"/></a>
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'trash-alt']"/></a>
+                            </div>
+                        </li>
+                        <li class="d-flex justify-content-between align-items-center pt-2 pb-1">
+                            <div class="d-inline-flex">
+                            <div class="form-check pl-0">
+                                <input type="checkbox" class="form-check-input" id="materialChecked7">
+                                <label class="form-check-label" for="materialChecked7"></label>
+                            </div>
+                            <p class="mb-0"><span class="text">Let theme shine like a star</span></p>
+                            </div>
+                            <div class="tools">
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'edit']"/></a>
+                            <a class="modtask"><font-awesome-icon :icon="['fas', 'trash-alt']"/></a>
+                            </div>
+                        </li>
+                        </ul>
+                    </div>
+                    <div class="card-footer white py-3">
+                        <div class="text-right">
+                            <button class="btn btn-primary btn-md px-3 my-0 mr-0">Add item<i class="fas fa-plus pl-2"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
     <section id="guests">
         <h4 class="font-weight-bold black-text mb-4 pb-2">Guests</h4>
@@ -91,6 +206,12 @@
 #blank, #deadlines, #tasks, #guests, #budget, #vendors, #partysection, .planningintro {
     height: 100vh;
     width: 100vw;
+}
+.page-item {
+    padding: 0;
+}
+.modtask {
+    padding: 2px;
 }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -4,14 +4,18 @@ import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
 import { faTasks } from '@fortawesome/free-solid-svg-icons'
 import { faUsers } from '@fortawesome/free-solid-svg-icons'
 import { faMoneyBill } from '@fortawesome/free-solid-svg-icons'
 import { faUtensils } from '@fortawesome/free-solid-svg-icons'
+import { faGlassCheers } from '@fortawesome/free-solid-svg-icons'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 
 
-library.add(faTasks,faUsers,faMoneyBill,faUtensils)
+library.add(faTasks,faUsers,faMoneyBill,faUtensils,faClock,faGlassCheers,faEdit,faTrashAlt)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('font-awesome-layers', FontAwesomeLayers)
