1200e7c5cef1a8a971dc375f5555f75e7db0cc88
Chasity Savella <csavella@pdx.edu>
2021-03-28 19:32:27 -0700

    Basic travel layout

--- src/components/details.vue
+++ src/components/details.vue
@@ -5,7 +5,7 @@
         <h2>Ceremony and Reception</h2>
         SATURDAY, SEPTEMBER 18, 2021<br>
         4:30 - 10:00 PM HST<br>
-        <a href="https://olowaluplantationhouse.com/"><h3>Olowalu Plantation House</h3></a>
+        <a class="venuelink" href="https://olowaluplantationhouse.com/"><h3>Olowalu Plantation House</h3></a>
         810 Olowalu Village Road<br>
         Lahaina, Hawaii 96761<br>
         <button class="btn btn-info" @click="newTab()">View Map</button>
@@ -58,10 +58,10 @@ button {
     padding: 20px;
     opacity: 0.6;
 }
-a {
+.venuelink {
   color: white;
 }
-a:hover {
+.venuelink:hover {
   color: white;
   text-decoration: underline;
 }
--- src/components/travel.vue
+++ src/components/travel.vue
@@ -1,68 +1,183 @@
 <template>
-  <div class="travel">
-    <div class="container greyblock">
-          <h2 class="temptitle">Ok, don't come at me. I'm still working on this. &#58;p</h2>
-          <p class="tempbody">
-    <!--      	• Tours/hiking:
-		○ Kapalua coastal trail
-			§ Free 
-			§ 2ish miles along the beach
-			§ Paved trail
-			§ https://www.yelp.com/biz/kapalua-coastal-trail-lahaina
-		○ Hana
-			§ Wai'anapanapa state park
-				□ Free
-				□ http://dlnr.hawaii.gov/dsp/parks/maui/waianapanapa-state-park/
-			§ 122 acres 
-			§ Black sand beach
-			§ Hana tube - tour of caves, red ti botanical maze, $12
-				□ http://www.mauicave.com/
-		○ Lahaina
-			§ Front street - restaurants, markets, and artwork
-			§ Banyan court - nice park next to beach with beautiful banyan tree
-		○ Molokini
-			§ Snorkeling or scuba diving off a boat
-			§ Price varies
-			§ Recommended company: http://www.mauidreamsdiveco.com/
-		○ Punakea palms farm
-			§ coconut farm + tasting
-			§ $49 each
-			§ https://www.punakeapalms.com/
-	• Favorite beaches
-		○ Ka'anapali beach
-		○ Kamaole (I, II, and III)
-		○ Wailea beach
-		○ Cove park - best for beginning surfing, lots of surf shops with rentals across the street
-	• Food:
-		○ Mama's fish house - very fancy seafood, very pricey but has amazing food and cocktails. Book reservation around time of sunset!, has amazing views, need to book 2 months in advance
-		○ Tin roof Maui - Hawaiian comfort food (not that fancy, but good food!)
-		○ Da kitchen café - Hawaiian food, great lunch spot, huge portions
-		○ Takamiya market - good grab-and-go Hawaiian food, poke, spam musubis, bento plates
-		○ Komoda's bakery - good guava + lilikoi malasadas and cream puffs
-		○ Guri guri - best sorbet ever! Call before heading over because hours are not accurate online
-		○ Poke hale grindz - great poke
-		○ Restaurants with nice views: Mama's fish house, Kimo's, Koa 156, the mill house
-		○ Pukalani Foodland - best poke on the island. (Don't go to any other Foodland for poke; it's not the same!)
-	• Nightlife
-		○ Kahale's
-		○ What Ales You
-		○ Vibe Bar
-		○ South Shore Tiki Lounge
-		○ Kimo's
-		○ Fleetwood's
-	• Luaus:
-		○ Check https://www.hawaiidiscount.com/maui/luaus.htm for discounts
-		○ Old lahaina lu'au - https://www.oldlahainaluau.com/. 
-			§ Details: dinner and drinks provided, imu ceremony. 
-			§ Entertainment: hula, tahitian, fire dancers. 
-			§ $125 per person (I know it's pricey, but all the luaus on Maui are usually >$100. :/ Highly recommend that you go to a luau at least once in your life though.)-->
-      </p>
+  <div class="travel text-muted">
+    <div class="container">
+		<div class="accommodations">
+			<h2>Accommodations</h2>
+			<p>We recommend that our guests find an Airbnb or hotel that's relatively close to our venue in Olowalu. 
+				Below are a few options in Lahaina and Ka'anapali. If you're on a budget, you can find cheaper places
+				to stay in Kihei, about 20-30 minute drive from Olowalu/Lahaina.</p>
+			<h3>Lahaina</h3>
+			<dl class="accommodationlist">
+				<dt>Puamana resort</dt>
+				<dd>• Gated condo resort, that has separate town homes and lots of common area with grassy areas, pool, 
+					etc. The price ranges depend on the individual owners and their level of upgrades done to their 
+					condo. This property is 5 minutes away from Olowalu.</dd>
+				<dd>• Website: <a class="travellink" href="http://www.aperfectvacationrental.com">http://www.aperfectvacationrental.com</a></dd>
+				<br>
+				<dt>Aina Nalu resort</dt>
+				<dd>• A lower end condo option, that is more like a hotel resort. This property is 10 minutes away from Olowalu.</dd>
+				<dd>• Website: <a class="travellink" href="http://www.outriggerainanalucondo.com/">http://www.outriggerainanalucondo.com/</a></dd>
+				<br>
+				<dt>Lahaina Shores Beach Resort</dt>
+				<dd>• Another option for a reasonable 'ocean front' Hotel</dd>
+				<dd>• Website: <a class="travellink" href="http://www.lahainashores.com/">http://www.lahainashores.com/</a></dd>
+			</dl>
+			<h3>Ka'anapali</h3>
+			<dl class="accommodationlist">
+				<dd><b>Hyatt Regency:</b> 
+					<a class="travellink"  href="https://www.hyatt.com/en-US/hotel/hawaii/hyatt-regency-maui-resort-andspa/oggrm"> 
+					https://www.hyatt.com/en-US/hotel/hawaii/hyatt-regency-maui-resort-andspa/oggrm
+					</a>
+				</dd>
+				<dd><b>Westin Maui resort and spa:</b> 
+				<a class="travellink" href="https://www.marriott.com/hotels/travel/hnmwi-the-westin-mauiresort-and-spa-kaanapali/"> 
+					https://www.marriott.com/hotels/travel/hnmwi-the-westin-mauiresort-and-spa-kaanapali/
+					</a>
+				</dd>
+				<dd><b>Marriott Maui Ocean Club:</b> 
+					<a class="travellink" href="https://www.marriott.com/hotels/travel/hnmmh-marriotts-mauiocean-club-molokai-maui-and-lanai-towers/"> 
+					https://www.marriott.com/hotels/travel/hnmmh-marriotts-mauiocean-club-molokai-maui-and-lanai-towers/
+					</a>
+				</dd>
+				<dd><b>Sheraton Maui Resort:</b> 
+					<a class="travellink" href="https://www.marriott.com/hotels/travel/hnmsi-sheraton-maui-resort-andspa/"> 
+					https://www.marriott.com/hotels/travel/hnmsi-sheraton-maui-resort-andspa/
+					</a>
+				</dd>
+			</dl>
+		</div>
+		<hr>
+		<div class="activities" id="activities">
+			<h2>Activities</h2>
+			<h3>Tours, Hiking, and Sights</h3>
+			<dl class="thingstodo">
+				<dt>Kapalua coastal trail</dt>
+				<dd>• 2ish miles along the beach, paved trail</dd>
+				<dd>• <a class="travellink" href="https://www.yelp.com/biz/kapalua-coastal-trail-lahaina">
+				https://www.yelp.com/biz/kapalua-coastal-trail-lahaina</a></dd>
+				<br>
+				<dt>Hana</dt>
+				<dd>• black sand beaches</dd>
+				<dd>• Hana tube - tour of caves, red ti botanical maze, $12</dd>
+				<dd>• <a class="travellink" href="http://www.mauicave.com/">
+				http://www.mauicave.com/</a></dd>
+				<br>
+				<dt>Wai'anapanapa state park</dt>
+				<dd>• huge park (120 acres) with panoramic view of black sand beach and green jungle</dd>
+				<dd>• ocean caves, cliff diving, fresh water pools, hiking trails, etc.</dd>
+				<dd>• <a class="travellink" href="http://dlnr.hawaii.gov/dsp/parks/maui/waianapanapa-state-park/">
+				http://dlnr.hawaii.gov/dsp/parks/maui/waianapanapa-state-park/</a></dd>
+				<br>
+				<dt>Lahaina</dt>
+				<dd>• Front street - restaurants, markets, and artwork</dd>
+				<dd>• Banyan court - nice park next to beach with beautiful banyan tree</dd>
+				<br>
+				<dt>Molokini</dt>
+				<dd>• Snorkeling or scuba diving off a boat</dd>
+				<dd>• Maui Snorkel Tours, $109<a class="travellink" href="https://mauisnorkeltours.com/tours/">
+				https://mauisnorkeltours.com/tours/</a></dd>
+				<dd>• Aqua Adventures, $115<a class="travellink" href="https://www.mauisnorkelsnuba.com/">
+				https://www.mauisnorkelsnuba.com/</a></dd>
+				<dd>• Molokini Snorkel Charters, $116<a class="travellink" href="https://mauisnorkelcharters.com/molokini-snorkel-tours/">
+				https://mauisnorkelcharters.com/molokini-snorkel-tours/</a></dd>
+				<br>
+				<dt>Punakea palms farm</dt>
+				<dd>• coconut farm + tasting, $49</dd>
+				<dd>• <a class="travellink" href="https://www.punakeapalms.com/">https://www.punakeapalms.com/</a></dd>
+				<br>
+			</dl>
+			<h3>Beaches</h3>
+			<dl class="beaches">
+				<dt>Ka'anapali beach</dt>
+				<dd>• most beautiful beach on the island</dd>
+				<br>
+				<dt>Honolua bay</dt>
+				<dd>• good for snorkeling and surfing</dd>
+				<br>
+				<dt>White rock (Palauea)</dt>
+				<dd>• nice spot for snorkeling and diving</dd>
+				<br>
+				<dt>Cove park</dt>
+				<dd>• best for beginning surfing, lots of surf shops with rentals across the street</dd>
+				<br>
+				<dt>Kapalua bay</dt>
+				<dd>• stop by to see some turtles</dd>
+				<br>
+				<dt>Ho'okipa</dt>
+				<dd>• experienced surfers only</dd>
+				<br>
+				<dt>Big beach in Makena</dt>
+				<dd>• waters are usually calm, good if you just want to chill and swim</dd>
+				<br>
+			</dl>
+			<h3>Food</h3>
+			<dl class="food">
+				<dt>Mama's fish house</dt>
+				<dd>• very fancy seafood, very pricey but has amazing food and cocktails. Book reservation around time of sunset!, 
+					has amazing views, need to book 2 months in advance</dd>
+				<br>
+				<dt>Tin roof Maui</dt>
+				<dd>• Hawaiian comfort food (not that fancy, but good food!)</dd>
+				<br>
+				<dt>Da kitchen café</dt>
+				<dd>• Hawaiian food, great lunch spot, huge portions</dd>
+				<br>
+				<dt>Takamiya market</dt>
+				<dd>• good grab-and-go Hawaiian food, poke, spam musubis, bento plates</dd>
+				<br>
+				<dt>Komoda's bakery</dt>
+				<dd>• best bakery on the island. Good guava + lilikoi malasadas and cream puffs</dd>
+				<br>
+				<dt>Guri guri</dt>
+				<dd>• sorbet. Call before heading over because hours are not accurate online</dd>
+				<br>
+				<dt>Poke hale grindz</dt>
+				<dd>• great poke</dd>
+				<br>
+				<dt>Restaurants with nice views</dt>
+				<dd>• Mama's fish house, Kimo's, Koa 156, the mill house</dd>
+				<br>
+				<dt>Pukalani Foodland</dt>
+				<dd>• best poke on the island</dd>
+				<dd>• Don't go to any other Foodland for poke; it's not the same!</dd>
+				<br>
+			</dl>
+			<h3>Nightlife</h3>
+			<dl class="nightlife">
+				<dd>Kahale's</dd>
+				<dd>What Ales You</dd>
+				<dd>Vibe Bar</dd>
+				<dd>South Shore Tiki Lounge</dd>
+				<dd>Kimo's</dd>
+				<dd>Fleetwood's</dd>
+			</dl>
+			<h3>Luaus</h3>
+			<dl class="luaus">
+				<dd>Check <a class="travellink" href="https://www.hawaiidiscount.com/maui/luaus.htm">https://www.hawaiidiscount.com/maui/luaus.htm</a> for discounts</dd>
+				<dd>Old lahaina lu'au, $125 <a class="travellink" href="https://www.oldlahainaluau.com/">https://www.oldlahainaluau.com/</a>.</dd>
+				<dd>Details: dinner and drinks provided, imu ceremony.</dd>
+				<dd>Entertainment: hula, tahitian, fire dancers.</dd>
+			</dl>
+		</div>
     </div>
   </div>
 </template>
 
 <style>
-.temptitle, .tempbody {
-  color: black;
+.travel {
+  background: linear-gradient(45deg, white, lightgrey);
 }
+
+.accommodations {
+	padding-top: 50px;
+}
+
+.travellink {
+	color: #665566;
+}
+.accommodationlist, .thingstodo, .beaches, .food, .nightlife, .luaus{
+	text-align: left;
+	list-style-type: circle;
+}
+
 </style>
